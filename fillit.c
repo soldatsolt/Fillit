6c5203280ed8f02dd881ef03746581799d3a378a
Kiana Mills <kmills@e2r10p5.21-school.ru>
2019-02-26 16:19:15 +0300

    Читаю и вывожу

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: kmills <kmills@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/25 21:53:22 by kmills            #+#    #+#             */
-/*   Updated: 2019/02/26 16:02:59 by kmills           ###   ########.fr       */
+/*   Updated: 2019/02/26 16:18:57 by kmills           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,19 @@
 
 int		main(int argc, char **argv)
 {
-    int     o;
-    int     r;
-    char    *buf;
+	int		o;
+	int		r;
+	char	buf[BUFF_SIZE + 1];
+	char	*str;
 
-
-    if (argc > 1)
-    o = open(argv[1], O_RDONLY);
-    while ((r = read()))
-    return (0);
+	str = ft_strnew(1);
+	if (argc > 1)
+	o = open(argv[1], O_RDONLY);
+	while ((r = read(o, buf, BUFF_SIZE)))
+	{
+		buf[r] = '\0';
+		str = ft_strrejoin(str, buf);
+	}
+	printf("%s\n", str);
+	return (0);
 }
