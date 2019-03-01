9cb4025651bfea87a4a5d7987c46ba5c3088a82b
Kiana Mills <kmills@e2r8p4.21-school.ru>
2019-03-01 21:23:35 +0300

    commit from vc

--- fillit.c
+++ fillit.c
@@ -23,3 +23,56 @@ int		main(int argc, char **argv)
 	printf("___%i___\n", r);
 	return (0);
 }
+
+int					kolvoresh(int o, int i, int n, unsigned short int *u)
+{
+	char	*str;
+	char	*karta;
+
+	karta = ft_strnew(1);
+	while (get_next_line(o, &str))
+	{
+		n = n + kolvoreshvstroke(str);
+		if (!((ft_strlen(str) == 4 && n < 5) || (n == 4 && !ft_strlen(str))))
+			return (0);
+		if (ft_strlen(str))
+			i++;
+		if (ft_strlen(str) == 0)
+		{
+			n = 0;
+			if (i != 4)
+				return (0);
+			i = 0;
+		}
+		karta = ft_strrejoin(karta, str);
+	}
+	u = tetramina(karta, 0, 0, ft_strlen(karta));
+	free(karta);
+	return (((i == 4) && u) ? 1 : 0);
+}
+
+unsigned short int	*tetramina(char *s, int k, int sym, int l)
+{
+	int					i;
+	unsigned short int	*u;
+
+	u = (unsigned short int *)malloc(sizeof(*u) * (l / 16));
+	ft_bzero(u, (l / 8));
+	i = 0;
+	while (k < (l / 16))
+	{
+		while (i < 16)
+		{
+			if (s[sym] == '#')
+			{
+				u[k] |= 1 << (16 - i - 1);
+			}
+			i++;
+			sym++;
+		}
+		k++;
+		i = 0;
+	}
+	return ((tetr_check(u, l, 1)) ? u : NULL);
+}
+
