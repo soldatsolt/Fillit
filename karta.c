76ebb646651a965069a20be0b27add1dadda0a8c
Kiana Mills <kmills@e2r8p4.21-school.ru>
2019-03-19 03:24:58 +0300

    БЫКТРЭКИНГ ГРАНИЦЫ))))

--- karta.c
+++ karta.c
@@ -6,7 +6,7 @@
 /*   By: kmills <kmills@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/01 21:16:24 by kmills            #+#    #+#             */
-/*   Updated: 2019/03/19 03:21:58 by kmills           ###   ########.fr       */
+/*   Updated: 2019/03/19 03:24:42 by kmills           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +82,7 @@ u_int64_t	zapoln_kartu(u_int64_t mapa, int i, u_int64_t llu, int min_size)
 	u_int64_t gran;
 
 	gran = ((u_int64_t)255 << ((7 - min_size) * 8));
+	gran |= ((u_int64_t)72340172838076673 << ((7 - min_size)));
 	naris(gran);
 	while ((mapa ^ (llu >> i)) != ((mapa | (llu >> i))))
 		i++;
