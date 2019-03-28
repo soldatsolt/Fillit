417ede4fede53a0ba67e6978b07c5a54eaaae5ad
Kiana Mills <kmills@e2r8p4.21-school.ru>
2019-03-28 13:00:11 +0300

    koordination

--- karta.c
+++ karta.c
@@ -6,7 +6,7 @@
 /*   By: kmills <kmills@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/01 21:16:24 by kmills            #+#    #+#             */
-/*   Updated: 2019/03/28 12:45:51 by kmills           ###   ########.fr       */
+/*   Updated: 2019/03/28 12:59:52 by kmills           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,28 @@ void		karta(u_int16_t *u, int l)
 	summ = (u_int16_t *)malloc(sizeof(u_int16_t) * 16);
 	ft_bzero(summ, 32);
 	g_size = min_map_size(l);
-	vstav_v_summ(&summ, u[0]);
+	vstav_v_summ(&summ, u[0], 5, 2);
 	naris_mass(summ);
 }
 
-void		vstav_v_summ(u_int16_t **sum, u_int16_t u)
+void		vstav_v_summ(u_int16_t **sum, u_int16_t u, int i, int j)
 {
 	u_int16_t	*summ;
 
 	summ = *sum;
-	summ[0] = u & 0xF000;
-	summ[1] = (u & 0x0F00) << 4;
-	summ[2] = (u & 0x00F0) << 8;
-	summ[3] = (u & 0x000F) << 12;
+	summ[j] = (u & 0xF000) >> i;
+	if (4 - i > 0)
+		summ[j + 1] = (u & 0x0F00) << (4 - i);
+	else
+		summ[j + 1] = (u & 0x0F00) >> (i - 4);
+	if (8 - i > 0)
+		summ[j + 2] = (u & 0x00F0) << (8 - i);
+	else
+		summ[j + 2] = (u & 0x00F0) >> (i - 8);
+	if (12 - i > 0)
+		summ[j + 3] = (u & 0x000F) << (12 - i);
+	else
+		summ[j + 3] = (u & 0x000F) >> (i - 12);
 }
 
 u_int64_t	*makethis(u_int64_t *tetr, int l, int k, u_int64_t tetrik)
